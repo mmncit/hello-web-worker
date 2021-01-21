1913598330056758484b4c406e57889a9d46ea5a
mamun <mamun.rashid@seequent.com>
2021-01-21 08:43:41 -0700

    add worker

---
+++ worker.js
@@ -0,0 +1,16 @@
+addEventListener("message", (d) => {
+  const imageData = d.data;
+  const w = imageData.width;
+  const h = imageData.height;
+  const data = imageData.data;
+
+  // Iterate pixel rows and columns to change red color of each.
+  for (let x = 0; x < w; x++) {
+    for (let y = 0; y < h; y++) {
+      let index = (x + y * w) * 4;
+      data[index] = data[index] * 1.2;
+    }
+  }
+
+  postMessage(imageData, [imageData.data.buffer]);
+});
